87b3f952bb1832c014a0d9425d5fce816c6a4bf5
egbuniwecemeka <egbuniwecemeka.com>
2024-12-08 16:12:31 +0100

    Redis utils

---
+++ main.js
@@ -0,0 +1,12 @@
+import RedisClient from './utils/redis';
+
+(async () => {
+  console.log(RedisClient.isAlive());
+  console.log(await RedisClient.get('myKey'));
+  await RedisClient.set('myKey', 12, 5);
+  console.log(await RedisClient.get('myKey'));
+
+  setTimeout(async () => {
+    console.log(await RedisClient.get('myKey'));
+  }, 1000*10)
+})();
