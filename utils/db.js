19875c8e4bdce9eb7332b1f70e8fa01e325e64d3
egbuniwecemeka <egbuniwecemeka.com>
2024-12-08 21:39:40 +0100

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+class DBClient {
+    constructor() {
+      const host = process.env.DB_HOST || 'localhost';
+      const port = process.env.DB_PORT || 27017;
+      const database = process.env.DB_DATABASE || 'files_manager';
+
+      const url = `mongodb://${host}:${port}`;
+
+      this.client = new MongoClient(url);
+      this.db = null;
+
+      this.client.connect()
+        .then(() => {
+          console.log("Successfully logged into MongoDB");
+          this.db = this.client.db(database);
+        }
+      ).catch((error) => {
+        console.error(`Login failed: ${error}`);
+      });
+    }
+}
