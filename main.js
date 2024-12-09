78b55c2b91c919d75be2bf7979d85b2bd51813a3
egbuniwecemeka <egbuniwecemeka.com>
2024-12-09 22:01:03 +0100

    Redis utils

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import redisClient from './utils/redis';
+import redisClient from "./utils/redis";
 
 (async () => {
   console.log(redisClient.isAlive());
-  console.log(await RedisClient.get('myKey'));
+  console.log(await redisClient.get('myKey'));
   await RedisClient.set('myKey', 12, 5);
-  console.log(await RedisClient.get('myKey'));
+  console.log(await redisClient.get('myKey'));
 
   setTimeout(async () => {
-    console.log(await RedisClient.get('myKey'));
+    console.log(await redisClient.get('myKey'));
   }, 1000*10)
 })();
