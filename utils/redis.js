176e794263500abde75fb8b21eb4b32921045aea
egbuniwecemeka <egbuniwecemeka.com>
2024-12-09 01:05:00 +0100

    MongoDB utils

--- utils/redis.js
+++ utils/redis.js
@@ -7,15 +7,17 @@ class RedisClient {
     constructor() {
         // Initialize the redis client
         this.client = createClient();
+
+        // Connect to redis server
+        // this.client.connect()
+        this.client.on('connect', () => {
+            console.log('Connection successful');
+        })
         // Listen and log errors
         this.client.on('error', (err) => {
             console.error(`Connection error: ${err}`);
         });
-        // Connect to redis server
-         this.client.connect()
-/*         this.client.on('connect', () => {
-            console.log('Connection successful');
-        }) */
+
 
         
     }
