be58ec781f4c9f1e19395ba93e50a8845404b41c
egbuniwecemeka <egbuniwecemeka.com>
2024-12-09 06:34:49 +0100

    MongoDB utils

--- utils/redis.js
+++ utils/redis.js
@@ -22,7 +22,42 @@ class RedisClient {
         
     }
 
-    /* s */
+    /* isAlive() {
+        console.log(this.client.isOpen ? true : false);
+        return this.client.isOpen ? true : false;
+    }
+
+    // asynchronous function for retrieving values of key
+    async get(key) {
+        try {
+          const res = await this.client.get(key);
+          console.log(res);
+          return res;
+        } catch (error) {
+          console.error(`Error while fetching ${key}: ${err}`);
+          return null;
+    }}
+
+    // asynchronous function for seeting key-value pairs after a certain duration
+    async set(key, value, time) {
+        try {
+          const res = await this.client.set(key, String(value), {'EX': time});
+          console.log(res);
+          return res;
+        } catch (err) {
+            console.log(`Error while setting ${key}: ${value} with expiration ${time}`);
+        }
+    }
+
+    // asynchronous function for deleting specified key-value pairs
+    async del(key) {
+        try {
+          const res = await this.client.del(key);
+          return res;
+        } catch (err) {
+            console.log(`Error while trying to delete ${key} with ${err} error`);
+        }
+    } */
 }
 
 export default new RedisClient();
