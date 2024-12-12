7431fb596f947c44ceb5fddef74dbb6b80eaddcc
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-12-12 15:52:36 -0800

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
 const express = require('express');
+const AppController = require('../controllers/AppController');
 const routes = express.Router();
 
 // endpoints for /status API
-routes.get('/status', (req, res) => {
-    res.status(200).send('Route endpoint for /status')
-});
+routes.get('/status', AppController.getStatus());
 
 // endpoints for /status API
-routes.get('/stats', (req, res) => {
-    res.status(200).json({stats: 'Route endpoint for /stats'});
-});
+routes.get('/stats', AppController.getStats());
 
+// Export routes/route
 module.exports = routes;
