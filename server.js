7cd5d9dbe8d672a9b0cb48386c7144afc9dbc4c8
egbuniwecemeka <egbuniwecemeka.com>
2024-12-12 23:53:34 +0100

    First API

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const express = require('express');
-const indexRoutes = require('./routes/index')
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Load routes from imported path.
-app.use('/', indexRoutes);
+app.get('/', (req, res) => {
+    res.send('Hello world')
+});
 
 // start server and Listen on specified port
-app.listen(port, () => {
+app.listen(port, (err) => {
+    if (err) {
+        console.error(`Error starting server: ${err}`);
+    }
     console.log(`Server is running at port: ${port}`);
 });
