81d9afd2289566c06da143eff6bd1992e21cb6b6
egbuniwecemeka <egbuniwecemeka.com>
2024-12-13 01:58:20 +0100

    First API

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const routes = express.Router();
 
 // Define routes and link them to cntroller methods
 routes.get('/status', AppController.getStatus);
-//routes.get('/stats', AppController.getStats);
+routes.get('/stats', AppController.getStats);
 
 // Export routes/route
 module.exports = routes;
