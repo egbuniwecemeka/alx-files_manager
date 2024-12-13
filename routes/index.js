79e2dd3ebde83463d768a9e951e8a9530daad272
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-12-12 16:10:20 -0800

    merge conflict

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const routes = express.Router();
 
-
 // Define routes and link them to cntroller methods
 routes.get('/status', AppController.getStatus);
 routes.get('/stats', AppController.getStats);
