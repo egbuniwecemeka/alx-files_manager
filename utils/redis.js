1ed0e6be75814791d3270c74fbf408eb32c7fb4c
egbuniwecemeka <egbuniwecemeka.com>
2024-12-17 02:18:12 +0100

    Redis utils eslint

--- utils/redis.js
+++ utils/redis.js
@@ -10,13 +10,13 @@ class RedisClient {
    */
   constructor() {
     this.client = createClient();
-    this.isClientConnected = true;
+    this.client.connected = true || false;
     this.client.on('error', (err) => {
       console.error('Redis client failed to connect:', err.message || err.toString());
-      this.isClientConnected = false;
+      this.client.connected = false;
     });
     this.client.on('connect', () => {
-      this.isClientConnected = true;
+      this.client.connected = true;
     });
   }
 
@@ -25,7 +25,7 @@ class RedisClient {
    * @returns {boolean}
    */
   isAlive() {
-    return this.isClientConnected;
+    return this.client.connected;
   }
 
   /**
